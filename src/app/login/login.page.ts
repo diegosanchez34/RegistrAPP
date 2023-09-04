2bfb8d050d60a171a30903ae46faf2a1be42237f
Diego <diegosanchez1234567@gmail.com>
2023-09-04 13:23:58 -0300

    creacion login

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  usuario = '';
+  clave = '';
+
+  constructor(private alertController: AlertController, private router: Router) { }
+
+  async Alerta() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Nombre de usuario invalido',
+      buttons: ['Cerrar'],
+    });
+
+    await alert.present();
+  }
+
+  verificar(){
+    if (this.usuario.length >= 3 && this.usuario.length  <= 8) {
+      this.irHome();
+    }
+    else {
+      this.Alerta();
+    }    
+  }
+  irHome(){
+    this.router.navigate(['/folder/Inbox']);
+  }
+
+  ngOnInit() {
+  }
+
+}
