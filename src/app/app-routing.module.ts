f6a6a7353fe268843021c0243d60d69ebd230e7e
Diego Alvarado, Joaquin Mendez, Jorge Massri <Jo.massri@duocuc.cl>
2023-09-07 15:59:30 -0300

    se agregan las carpetas de las pagina del sidemenu para navegar y se agrega el footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,24 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'inicio',
+    loadChildren: () => import('./users/inicio/inicio.module').then( m => m.InicioPageModule)
+  },
+  {
+    path: 'micuenta',
+    loadChildren: () => import('./users/micuenta/micuenta.module').then( m => m.MicuentaPageModule)
+  },
+  {
+    path: 'qr',
+    loadChildren: () => import('./users/qr/qr.module').then( m => m.QrPageModule)
+  },  {
+    path: 'ajustes',
+    loadChildren: () => import('./users/ajustes/ajustes.module').then( m => m.AjustesPageModule)
   }
+
+
 ];
 
 @NgModule({
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -33,7 +33,7 @@ export class LoginPage implements OnInit {
     }    
   }
   irHome(){
-    this.router.navigate(['/folder/Inbox']);
+    this.router.navigate(['inicio']);
   }
 
   ngOnInit() {
