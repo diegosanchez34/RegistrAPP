34bea96e0a0abd2cb97e469b818028dbe153b39d
diego <diegosanchez1234567@gmail.com>
2023-10-16 10:59:55 -0300

    generar servicio API

---
+++ src/app/api.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { retry, catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+    httpOptions = {
+      headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin' :'*'
+      })
+    }
+
+    // Se establece la base url del API a consumir
+    apiURL = 'https://jsonplaceholder.typicode.com';
+
+   
+  constructor(private http:HttpClient) { }
+
+  getPosts():Observable<any>{
+      return this.http.get(this.apiURL+'/posts/').pipe(
+      retry(3)
+      );
+    }
+     
+   
+}
