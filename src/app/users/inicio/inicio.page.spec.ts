95adaffd428f74a4e065156aa530177768ef7477
diego <diegosanchez1234567@gmail.com>
2023-11-26 18:36:42 -0300

    tema claro

--- src/app/users/inicio/inicio.page.spec.ts
+++ src/app/users/inicio/inicio.page.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { InicioPage } from './inicio.page';
 
 describe('InicioPage', () => {
--- src/app/users/profesores/profesores.page.ts
+++ src/app/users/profesores/profesores.page.ts
@@ -14,6 +14,7 @@ export class ProfesoresPage implements OnInit {
   }
 
   ngOnInit() {
+    
   }
 
 }
